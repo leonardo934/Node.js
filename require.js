cc1a674b1817b802f165a817088a41bd035a9727
leonardo <leonardocave934@gmail.com>
2023-03-13 19:32:21 -0300

    Pegando informações do processo

--- require.js
+++ require.js
@@ -1,2 +1,16 @@
 //console.log(require('path'))
-console.log(process.argv)
+//console.log(process.argv)
+
+
+
+
+
+//modulos nativos
+
+// const path = require('path')
+
+// console.log(path.basename(__filename))
+
+//meus modulos
+const myModule = require('./exports')
+console.log(myModule)
