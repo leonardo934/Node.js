23be7ac71a133b6d82b0afd8ee5e9f1a90b75cd0
leonardo <leonardocave934@gmail.com>
2023-03-13 20:17:44 -0300

    Iniciando um pacote com npm

--- process.js
+++ process.js
@@ -1,5 +1,7 @@
-// console.log(process.argv)
-const firstName = process.argv[2]
-const lastName = process.argv[3]
+ console.log(process.argv)
 
-console.log(`Seu nome é ${firstName} ${lastName}`)
+
+// const firstName = process.argv[2]
+// const lastName = process.argv[3]
+
+// console.log(`Seu nome é ${firstName} ${lastName}`)
